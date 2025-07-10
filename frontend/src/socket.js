46c34aac8304ce460e5a8bfc0d2e4f0362789a7d
Rudra Jani <141865862+algoamigoo@users.noreply.github.com>
2025-07-10 09:41:06 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,13 +1,16 @@
 import { io } from 'socket.io-client';
 
 export const initSocket = async () => {
-    console.log('Socket.IO connecting to:', process.env.REACT_APP_BACKEND_URL);
+    const url = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5001'; 
+    console.log('Socket.IO connecting to:', url);
+
     const options = {
-        'force new connection': true,
-        reconnectionAttempt: 'Infinity',
+        forceNew: true, 
+        reconnection: true, 
+        reconnectionAttempts: Infinity,
         timeout: 10000,
         transports: ['websocket'],
     };
-    const url = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5001'; // Fallback
+
     return io(url, options);
 };
